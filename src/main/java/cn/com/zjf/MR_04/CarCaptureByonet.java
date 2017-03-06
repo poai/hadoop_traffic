4ac4204dc0d4d3bdf02feb4db6fd26f588888790
poai <247690790@qq.com>
2017-03-06 12:09:23 +0800

    从原始数据中清洗出所有的卡口ID

--- src/main/java/cn/com/zjf/MR_04/CarCaptureByonet.java
+++ src/main/java/cn/com/zjf/MR_04/CarCaptureByonet.java
@@ -16,19 +16,19 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.CombineTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
 
-public class Car3 {
+public class CarCaptureByonet {
 
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
-		Job job = Job.getInstance(conf, "JobName");
-		job.setJarByClass(cn.com.zjf.MR_04.Car3.class);
+		Job job = Job.getInstance(conf, "ITGS_LIST");
+		job.setJarByClass(cn.com.zjf.MR_04.CarCaptureByonet.class);
 		job.setInputFormatClass(CombineTextInputFormat.class);
+		job.setMapperClass(CarMapper.class);
 		job.setReducerClass(CarReduce.class);
 		job.setMapOutputValueClass(Text.class);
-		job.setOutputKeyClass(Text.class);
-		
+		job.setMapOutputKeyClass(Text.class);
+		job.setOutputKeyClass(NullWritable.class);
 		FileSystem fs = FileSystem.get(conf);
 		Path input = new Path(args[0]);
 		/**
@@ -73,10 +73,8 @@ class CarMapper extends Mapper<LongWritable, Text, Text, Text> {
 			temp = temp.substring(12);
 			String[] items = temp.split(",");
 			if (items.length > 10) {
-				// CarPlate As Key
-				if (!items[2].endsWith("无牌")) {
-					context.write(new Text(items[14].substring(6)), value);
-				}
+				// 卡口ID
+				context.write(new Text(items[14].substring(6)), value);
 			}
 		}
 
@@ -84,27 +82,10 @@ class CarMapper extends Mapper<LongWritable, Text, Text, Text> {
 }
 
 class CarReduce extends Reducer<Text, Text, NullWritable, Text> {
-	private MultipleOutputs<NullWritable, Text> mo;
-
 	@Override
-	protected void reduce(Text key, Iterable<Text> vlaues, Reducer<Text, Text, NullWritable, Text>.Context arg2)
+	protected void reduce(Text key, Iterable<Text> vlaues, Reducer<Text, Text, NullWritable, Text>.Context context)
 			throws IOException, InterruptedException {
-		for (Text text : vlaues) {
-			if (text != null) {
-				mo.write(NullWritable.get(), text, key.toString());
-			}
-		}
+		context.write(NullWritable.get(), key);
 	}
 
-	@Override
-	protected void setup(Reducer<Text, Text, NullWritable, Text>.Context context)
-			throws IOException, InterruptedException {
-		mo = new MultipleOutputs<NullWritable, Text>(context);
-	}
-
-	@Override
-	protected void cleanup(Reducer<Text, Text, NullWritable, Text>.Context context)
-			throws IOException, InterruptedException {
-		mo.close();
-	}
 }
